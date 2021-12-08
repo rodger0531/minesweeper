a33adba8bd44b041f10b37ca11bc0af9c926ee57
Rodger <rodger0531@gmail.com>
2021-12-08 22:53:32 +0800

    add mine grid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 // import "./App.css";
 
 function App() {
+  const onClickBlock = () => {
+    console.log("clicked");
+  };
+
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-gray-900">
-      <div className="flex items-center justify-center w-96 h-96 bg-gray-400 rounded ">
-        Content
+      <div className="flex items-center justify-center bg-gray-100 p-12 rounded ">
+        <div className="grid grid-cols-10">
+          {new Array(10 * 10).fill().map((_, idx) => (
+            <div
+              key={idx}
+              className="h-6 w-6 m-px bg-gray-400 shadow-inner"
+              onClick={onClickBlock}
+            ></div>
+          ))}
+        </div>
       </div>
     </div>
   );
